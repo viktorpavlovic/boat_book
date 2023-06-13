5ff8c152fe8069d0ce1b009b17fc3806981ea056
grbick <grbic.konstantin@gmail.com>
2023-06-14 01:58:40 +0200

    admin tour mapping update

--- src/Components/AdminTours/index.jsx
+++ src/Components/AdminTours/index.jsx
@@ -1,4 +1,4 @@
-import { React, useContext } from "react";
+import { React, useContext, useState } from "react";
 import { applicationContext } from "../../context";
 import { doc, deleteDoc } from "firebase/firestore";
 import { db } from "../../firebase";
@@ -7,20 +7,48 @@ import "./admin-tours.scss";
 
 const AdminTours = ({ handleOpen }) => {
   const { allDocs, freshData, setFreshData } = useContext(applicationContext);
+  const [pastTours, setPastTours] = useState(false);
   const handleDelete = async (e) => {
     await deleteDoc(doc(db, "tours", e.id));
     setFreshData(!freshData);
   };
+  const filteredDocs = allDocs
+    .sort((a, b) =>
+      pastTours
+        ? moment(b.data.date) - moment(a.data.date)
+        : moment(a.data.date) - moment(b.data.date)
+    )
+    .filter((e) =>
+      pastTours
+        ? moment(e.data.date) < moment(new Date())
+        : moment(e.data.date) >= moment(new Date())
+    );
   return (
     <div className="div-admin-tours">
+      <div className="past-toggle-div">
+        <button
+          className={pastTours ? "selected" : ""}
+          onClick={() => setPastTours(!pastTours)}
+        >
+          Past
+        </button>
+        <button
+          className={pastTours ? "" : "selected"}
+          onClick={() => setPastTours(!pastTours)}
+        >
+          Upcoming
+        </button>
+      </div>
       <main>
-        {allDocs?.sort((a,b)=>moment(a.data.date)-moment(b.data.date)).map((e) => (
-            <section key={e.id}>
-              <p>{moment(e?.data?.date).format("LL")}</p>
-              <p>{e.data.time}</p>
-              <button onClick={() => handleOpen(e)}>Tour Info</button>
-              <button className="del" onClick={() => handleDelete(e)}>Delete</button>
-            </section>
+        {filteredDocs.map((e) => (
+          <section key={e.id}>
+            <p>{moment(e?.data?.date).format("LL")}</p>
+            <p>{e.data.time}</p>
+            <button onClick={() => handleOpen(e)}>Tour Info</button>
+            <button className="del" onClick={() => handleDelete(e)}>
+              Delete
+            </button>
+          </section>
         ))}
       </main>
     </div>
